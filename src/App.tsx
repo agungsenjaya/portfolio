28ae865390d3f9cbd6cb878b6159395228755b98
agungsenjaya <agungsenjaya813@gmail.com>
2026-04-09 22:15:00 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,83 @@
-import React from 'react'
-import Header from './components/header'
+import React, { useEffect, useRef } from "react";
+import Header from "./components/header";
+import { Button } from "./components/tailgrids/core/button";
+import gsap from "gsap";
 
 export default function App() {
+  const splitRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!splitRef.current) return;
+
+    const chars = splitRef.current.querySelectorAll(".split-char");
+
+    gsap.fromTo(
+      chars,
+      { opacity: 0, y: 80, rotateX: -90 },
+      {
+        opacity: 1,
+        y: 0,
+        rotateX: 0,
+        duration: 0.8,
+        stagger: 0.03,
+        ease: "back.out(1.7)",
+      }
+    );
+  }, []);
+
+  function splitText(text: string) {
+    return text.split("").map((char, i) => (
+      <span
+        key={i}
+        className="split-char inline-block"
+        style={{ perspective: "400px" }}
+      >
+        {char === " " ? "\u00A0" : char}
+      </span>
+    ));
+  }
+
   return (
-    <div>
-      <Header/>
+    <>
+      <Header />
       <section>
-        {/*  */}
+        <div className="relative">
+          <div className="container grid grid-cols-3 z-20 relative">
+            <div className="self-end space-y-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Deserunt, numquam sed eos quibusdam fugit nostrum cupiditate,
+                voluptas nemo ratione necessitatibus dignissimos vitae earum
+                atque rem quia iure placeat veritatis consectetur!
+              </p>
+              <Button>Look at</Button>
+            </div>
+            <div className="relative">
+              <img src="/img/foto.png" className="w-full" alt="" />
+              <div className="absolute bottom-0 left-0 right-0 h-[400px] bg-linear-to-t from-dark from-15% to-dark/0" />
+            </div>
+            <div className="self-end space-y-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Deserunt, numquam sed eos quibusdam fugit nostrum cupiditate,
+                voluptas nemo ratione necessitatibus dignissimos vitae earum
+                atque rem quia iure placeat veritatis consectetur!
+              </p>
+            </div>
+          </div>
+          <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center z-10">
+            <div ref={splitRef} className="space-y-2 w-full container">
+              <h2>Since 2017</h2>
+              <h1 className="text-8xl font-semibold text-start">
+                {splitText("Software")}
+              </h1>
+              <h1 className="text-8xl font-semibold text-end">
+                {splitText("Engineer")}
+              </h1>
+            </div>
+          </div>
+        </div>
       </section>
-    </div>
-  )
+    </>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <header className='my-4'>
-      <div className='mx-auto max-w-6xl'>
-        <div className='bg-gray-200 p-4 flex justify-between rounded-full'>
-          <div>
-            Agung Senjaya
-          </div>
-          <div>
-            <ul className='flex gap-8'>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Skills</a>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-              <li>
-                <a href="#">Github</a>
-              </li>
-            </ul>
-          </div>
+    <header className="py-4">
+      <div className="mx-auto max-w-4xl flex justify-between">
+        <div>
+          <h4 className="font-bold text-xl">Agung Senjaya</h4>
         </div>
+        <ul className='flex gap-18'>
+        <li className='underline underline-offset-8'>
+          <a href="#about">
+            About
+          </a>
+        </li>
+        <li className='underline underline-offset-8'>
+          <a href="#project">
+            Portfolio
+          </a>
+        </li>
+        <li className='underline underline-offset-8'>
+          <a href="">
+            Contact Us
+          </a>
+        </li>
+        <li className='underline underline-offset-8'>
+          <a href="">
+            Github
+          </a>
+        </li>
+      </ul>
       </div>
     </header>
-  )
+  );
 }
